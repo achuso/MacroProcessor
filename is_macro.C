b7193229012745c91fb2a2298ea4cebe4652d531
mahnyb <mahnybarazandehtar1998@gmail.com>
2023-05-17 04:14:06 +0300

    changed the if (incomplete) and wrote the else
    Signed-off-by: mahnyb <mahnybarazandehtar1998@gmail.com>

--- is_macro.C
+++ is_macro.C
@@ -1,18 +1,21 @@
 #include <stdio.h>
 #include <string.h>
 
+void expand(); 
 
 void is_macro(const char* line){
 
     // check if the line starts with a macro name or an #if
 
-    if(strncmp(line, "#if", 3 ) || ){
+    if(strncmp(line, "#if", 3) == 0 || strncmp(line, "#ADD", 4) == 0 || strncmp(line, "#MULT", 5) == 0 || strncmp(line, "#DIV", 4) == 0 || strncmp(line, "#REM", 4) == 0){
 
-        //expand(macro_name);
+        expand(); // add the macro name inside 
 
     }
     else{
         //write the line into the .asm file
+        FILE* f1 = fopen("f1.asm", "a"); // creates/opens the f1.asm files and "a" apends data into the file
+        fprintf(f1, "%s\n", line); // writes the contents of the line into f1
     }
 
     
