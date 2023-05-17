a425b84d6d9c1a3c1fd4d9be606c37b02500c894
mahnyb <mahnybarazandehtar1998@gmail.com>
2023-05-17 19:24:44 +0300

    optimized the if statement (incomplete) Signed-off-by: mahnyb <mahnybarazandehtar1998@gmail.com>

--- is_macro.C
+++ is_macro.C
@@ -5,11 +5,20 @@ void expand();
 
 void is_macro(const char* line){
 
-    // check if the line starts with a macro name or an #if
+    // check if the line starts with an #if
 
-    if(strncmp(line, "#if", 3) == 0 || strncmp(line, "ADD", 3) == 0 || strncmp(line, "SUB", 3) == 0 || strncmp(line, "MULT", 4) == 0 || strncmp(line, "DIV", 3) == 0 || strncmp(line, "REM", 3) == 0){
+    if(strncmp(line, "#if", 3) == 0){
+    
+    // check for conditions
+    // format: #if $2 ABC M3 200 300 
+    // $2 is a condition parameter that is given as an argument such that arg[2]
+    // get the number after the $, pass it into arg[], and check if its equal to the thing inside the paranthesis    
+    
+    }
+    // check if the line starts with a macro name
+    else if (strncmp(line, "ADD", 3) == 0 || strncmp(line, "SUB", 3) == 0 || strncmp(line, "MULT", 4) == 0 || strncmp(line, "DIV", 3) == 0 || strncmp(line, "REM", 3) == 0){ 
 
-        expand(); // add the macro name inside 
+            expand(); // expands the macro        
 
     }
     else{
