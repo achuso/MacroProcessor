902816453937416c61d52f1760854b6fcd47f4aa
EmirErtorer <115039531+EmirErtorer@users.noreply.github.com>
2023-05-18 17:42:41 +0300

    major changes to the Expand.c
    may need more tweaking for performance only

--- Expand.c
+++ Expand.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 struct mac {
     char mname[8];
@@ -6,46 +7,50 @@ struct mac {
     char macro[256];
 };
 
-struct pt {
-    char mname[8];
-    int nparams;
-    char dummy[10][4];
-    char actual[10][4];
-};
-
-void expand(char field[10][7], struct mac buffer[10], int m_count){
-    struct pt PT;
-    createPT(field, buffer, m_count);
-
-    // Take a line from the macro body and write it to the expanded code
-    // If there are dummy parameters in that line, substitute them
-    // with their actual parameters using the parameter table
-    // Assuming 'line' represents the line from the macro body
-    char expandedLine[256]; // Buffer to store the expanded line
-    int i = 0; // Index for the expandedLine buffer
+void expand(){
+    createPT(field, buffer, m_count);  
+    // take a line from the macro body
+    // check if there are dummy parameters
+        //if there are, substitue them with the actual parameters using the parameter table
+    // and append it to f1.asm
     
-    // Iterate over each character in the line
-    for (int j = 0; line[j] != '\0'; j++) {
-        // Check if the current character is a dummy parameter
-        if (line[j] == '$') {
-            // get the parameter index from the line
-            int paramIndex = j + 1;
-            
-            // Substitute the dummy parameter with the actual parameter
-            strcpy(&expandedLine[i], PT.actual[paramIndex]);
-            
-            // Update the index i and skip the remaining characters of the dummy parameter
-            i += strlen(PT.actual[paramIndex]);
-            j += 2; // Skip the '$' and the digit
-        } else {
-            // Copy the current character to the expanded line
-            expandedLine[i++] = line[j];
+    // !!! MAY MODIFY THIS TO PASS THE FILE f1 AS PARAMETER INSTEAD, CHECK WITH MAHNY
+    FILE* f1 = fopen("f1.asm", "a"); // opens the file f1
+
+    if (file == NULL) {
+        printf("Failed to open .asm file.\n");
+        return;
+    } 
+    // iterate and find the macro definition for the current macro.
+    struct mac* macroDef = NULL;
+    for(int i = 0; i < m_count; i++) {
+        if (strcmp(buffer[i].mname, PT.mname) == 0) {
+            macroDef = &buffer[i];
+            break;
         }
+    }    
+    // If the macro definition was not found return.
+    if (macroDef == NULL) {
+        printf("Macro definition not found.\n");
+        return;
     }
     
-    // Null-terminate the expanded line
-    expandedLine[i] = '\0';
-
-    // havent yet figured out if the expanded line should be returned for the is_macro() to print or should 
-    // this function print
+    // For each line in the macro definition.
+    char* line = strtok(macroDef->macro, "\n"); // goes line by line through the macro body
+    while (line != NULL) {
+        // For each dummy parameter in the parameter table.
+        for(int i = 0; i < PT.nparams; i++) {
+            // Replace all occurrences of the dummy parameter in the line with its corresponding actual parameter.
+            while ((line = strstr(line, PT.dummy[i])) != NULL) { // searches for the first occurrence of the dummy parameter in the current line.
+                // copies the actual parameter over the dummy parameter in the line
+                strncpy(line, PT.actual[i], strlen(PT.actual[i]));
+                // Move the line pointer forward by the length of the actual parameter.
+                line += strlen(PT.actual[i]);
+            }
+        }       
+        // Write the line to the .asm file.
+        fprintf(f1, "%s\n", line);  
+        line = strtok(NULL, "\n"); //  gets the next line 
+    }
+    fclose(f1);
 }
