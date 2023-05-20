da4eff297661b58ac4da2695402ffbc549179d27
achuso <onatribar@protonmail.ch>
2023-05-20 21:56:27 +0300

    initial commit

---
+++ group5main.c
@@ -0,0 +1,11 @@
+// CSE 232 Group 5
+// Term Project - Macroprocessors
+// Onat Ribar
+// Mahny Barazandehtar
+// Ayşegül Tekeli
+// Ece Tipici
+// Emir Devlet Ertörer 
+
+#include <stdio.h>
+#include <string.h>
+
