27e023e49386c71f11c20c60bc95310aeb975688
mahnyb <mahnybarazandehtar1998@gmail.com>
2023-05-20 22:53:06 +0300

    completed the if statement

--- is_macro.C
+++ is_macro.C
@@ -3,22 +3,34 @@
 
 void expand(); 
 
-void is_macro(const char field[10][7], char* filename){  
+void is_macro(const char field[10][7], char* filename, char** argv){  
 
-    // check if the line starts with an #if
+    // check if the line starts with an if
 
-    if(strncmp(field[0], "if", 2)){ 
-    
-    // check for conditions
-    // format: #if $2 ABC M3 200 300 
+    if(strncmp(field[0], "if", 2)){
+
+
+    // check for conditions:
+    // example: field format: if $2 ABC M3 200 300 
     // $2 is a condition parameter that is given as an argument such that arg[2]
-    // get the number after the $, pass it into arg[], and check if its equal to the thing inside the paranthesis    
+    // get the number after the $, pass it into arg[], and check if its equal to the thing inside the paranthesis
+
+        int argVal = field[1][1]; // to extract the number after the $
+
+        if (argv[argVal] == field[2]){
+
+            expand();
+            
+        }
+        else{
+            return;
+        }
     
     }
     // check if the line starts with a macro name
     else if (strncmp(field[0], "ADD", 3) == 0 || strncmp(field[0], "SUB", 3) == 0 || strncmp(field[0], "MULT", 4) == 0 || strncmp(field[0], "DIV", 3) == 0 || strncmp(field[0], "REM", 3) == 0){ 
 
-            expand(); // expands the macro        
+            expand();        
 
     }
     else{
