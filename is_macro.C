0f381570d38844a5b679dd67980cabb18877fb9c
mahnyb <mahnybarazandehtar1998@gmail.com>
2023-05-20 21:58:32 +0300

    changed line into a string array

--- is_macro.C
+++ is_macro.C
@@ -3,11 +3,11 @@
 
 void expand(); 
 
-void is_macro(const char* line){
+void is_macro(const char field[10][7], char* filename){  // replace line with string array
 
     // check if the line starts with an #if
 
-    if(strncmp(line, "#if", 3) == 0){
+    if(strncmp(field[0], "if", 2)){ // field[0] == "if"
     
     // check for conditions
     // format: #if $2 ABC M3 200 300 
@@ -16,15 +16,22 @@ void is_macro(const char* line){
     
     }
     // check if the line starts with a macro name
-    else if (strncmp(line, "ADD", 3) == 0 || strncmp(line, "SUB", 3) == 0 || strncmp(line, "MULT", 4) == 0 || strncmp(line, "DIV", 3) == 0 || strncmp(line, "REM", 3) == 0){ 
+    else if (strncmp(field[0], "ADD", 3) == 0 || strncmp(field[0], "SUB", 3) == 0 || strncmp(field[0], "MULT", 4) == 0 || strncmp(field[0], "DIV", 3) == 0 || strncmp(field[0], "REM", 3) == 0){ 
 
             expand(); // expands the macro        
 
     }
     else{
         //write the line into the .asm file
-        FILE* f1 = fopen("f1.asm", "a"); // creates/opens the f1.asm files and "a" apends data into the file
-        fprintf(f1, "%s\n", line); // writes the contents of the line into f1
+        FILE* f1 = fopen(filename, "a"); // creates/opens the filename and "a" apends data into the file
+        
+        for(int i = 0; field[i][0] != NULL; i++){
+
+            fprintf(f1, "%s ", field[i]); // writes the contents of the line into f1
+
+        }
+
+        fprintf(f1, "\n");
     }
 
     
